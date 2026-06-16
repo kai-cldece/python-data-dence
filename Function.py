5192537d0f17059c837d382bf4f5c8c3ac418517
KayyCL <benedictfusin99@gmail.com>
2026-06-17 01:22:09 +0800

    Create Function.py

---
+++ Function.py
@@ -0,0 +1,10 @@
+#In data analysis, we the programmers often calculate the same iteration for a hundred of times.   Instead of re-writing the code, we will be using the Function. 
+
+# Defining the function
+def calculate_profit(revenue, expenses):
+    profit = revenue - expenses
+    return profit
+
+# Using the function later in your code
+monday_profit = calculate_profit(500, 200)
+print(monday_profit) # Output: 300
