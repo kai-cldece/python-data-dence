75f4962fa628a6b79356e563b68b106cb8c0ed9b
KayyCL <benedictfusin99@gmail.com>
2026-06-19 23:33:30 +0800

    Update introduction to data exercise 2.py

--- Introduction to Data/introduction to data exercise 2.py
+++ Introduction to Data/introduction to data exercise 2.py
@@ -1,3 +1,17 @@
+"""
+The task.
+You have a temperature sensor outside your shop. Most of the time it works, but occasionally it glitches and records a temperature of -999. You need to clean this data before you can analyze it.
+
+Objectives:
+1. Create a list called raw_temps with these numbers: 72, 74, -999, 75, 71, -999, 73.
+2. Create a new, empty list called clean_temps. (Hint: just use empty brackets []).
+Write a for loop to iterate through raw_temps.
+3. Inside the loop, write an if statement: If the temperature is not equal to -999, append (add) that temperature to your clean_temps list.
+4. Print the clean_temps list. It should no longer contain any errors!
+
+"""
+
+
 # 1. A list containing multiple dictionaries
 customers = [
     {"name": "Alice", "tier": "VIP", "spent": 150.00},
